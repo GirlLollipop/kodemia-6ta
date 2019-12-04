4f5fffd46e17767bc5b3be0752b0a622a3a1cf09
GirlLollipop <girl.lollipop@gmail.com>
2019-12-03 20:37:25 -0600

    ejercicio operadores logicos

--- js/main.js
+++ js/main.js
@@ -3,3 +3,10 @@ var word = "perros"
 var singWord = word[0]+word[1]+word[2]+word[3]+word[4]
 
 console.log(singWord)
+
+
+var number = 100
+
+if (number >= 0 && number <= 100 && number % 10 === 0 ){
+    console.log("El número estas en el rango")
+}
