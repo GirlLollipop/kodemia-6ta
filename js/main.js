49bec3f358dca5d193b68719bc6df4c887fc8b2a
GirlLollipop <girl.lollipop@gmail.com>
2019-12-03 20:41:45 -0600

    ejercicio con else

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ var singWord = word[0]+word[1]+word[2]+word[3]+word[4]
 console.log(singWord)
 
 
-var number = 100
+var number = 200
 
 if (number >= 0 && number <= 100 && number % 10 === 0 ){
-    console.log("El número estas en el rango")
+    console.log("El número esta en el rango")
+} else {
+    console.error("No esta en el rango permitido")
 }
